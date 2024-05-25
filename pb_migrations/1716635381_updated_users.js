3df60c6dfa7b82696d89a6e95b14c73ebd78c059
ram <justforthegreatergood@gmail.com>
2024-05-25 19:10:07 +0800

    added pocketbase

---
+++ pb_migrations/1716635381_updated_users.js
@@ -0,0 +1,96 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("users_avatar")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "7teoqwq0",
+    "name": "age",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "piffqstg",
+    "name": "salary",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "j4pml7as",
+    "name": "balance",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "users_avatar",
+    "name": "avatar",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/jpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/gif",
+        "image/webp"
+      ],
+      "thumbs": null,
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("7teoqwq0")
+
+  // remove
+  collection.schema.removeField("piffqstg")
+
+  // remove
+  collection.schema.removeField("j4pml7as")
+
+  return dao.saveCollection(collection)
+})
